48a02a31f28dbb54d7b553837d2624c78f401e6d
lvercelli <luis.vercelli@culqi.com>
2016-05-04 17:28:58 -0500

    Inicio del proyecto

---
+++ culqi_sample/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for culqi_sample project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from mezzanine.utils.conf import real_project_name
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "%s.settings" % real_project_name("culqi_sample"))
+
+application = get_wsgi_application()
